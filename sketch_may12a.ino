978f3085f7ad50b74c2215351857195f036d3cab
HimathHarshajith <30776856+HimathHarshajith@users.noreply.github.com>
2017-10-01 17:58:27 +0530

    Add files via upload

---
+++ sketch_may12a.ino
@@ -0,0 +1,21 @@
+void setup() {
+  pinMode(4,INPUT);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+
+}
+
+void loop() {
+
+
+ if(digitalRead(4)==LOW){
+  digitalWrite(11,HIGH);
+  digitalWrite(12,LOW);
+ }
+
+ else{
+  digitalWrite(12,HIGH);
+  digitalWrite(11,LOW);
+ }
+
+}
